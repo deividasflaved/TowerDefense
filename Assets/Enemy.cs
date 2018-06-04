35046cf19d11a09e6ea009f8630ebf889975d0d4
DESKTOP-A0IJAUH\Deividas <deividas.seimys@gmail.com>
2018-06-04 15:27:11 +0300

    Initial

---
+++ Assets/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 1f; //Judėjimo greitis
+
+    private Transform target; //Sekantis taškas kur judėt
+    private int wavepointIndex = 0; //Sekančio taško index
+
+    void Start()
+    {
+        target = Waypoints.points[0];//Pradžiai parenkam pirma tašką
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;//Gaunam kryptį
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);//Judėjimas
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.02f)//Jei pasiekėm tašką paimtų kitą
+            GetNextWaypoint();
+    }
+    /// <summary>
+    /// kito taško paėmimas
+    /// </summary>
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
